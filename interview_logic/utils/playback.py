64c321e9185b026c6fc133333e2dde38ed4cdfb3
Jack Ferreri <166726580+jack-ferreri12@users.noreply.github.com>
2025-05-16 14:15:45 -0400

    Delete interview_logic directory

--- interview_logic/utils/playback.py
+++
@@ -1,9 +0,0 @@
-import simpleaudio as sa
-
-def play_audio(filename: str):
-    try:
-        wave_obj = sa.WaveObject.from_wave_file(filename)
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
-    except Exception as e:
-        print(f"[Error playing {filename}]:", e)
